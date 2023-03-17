cbfc7ea1f1f290b1c95672816d2d0814f8591932
Reinis Rudens <reinis.rudens.dev@gmail.com>
2023-03-17 23:31:52 +0200

    Working OAuth

--- config/config.exs
+++ config/config.exs
@@ -62,3 +62,12 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: {Ueberauth.Strategy.Github, []}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "60645299546875f631e6",
+  client_secret: "40c61f45009bebc8288040571b1a3baa933ac4d3"
